d56c81d6ab50ea38a09a98b96deb8aebc24d4f09
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2024-11-07 13:50:10 -0500

    show code by default

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,18 @@
-import type { Preview } from "@storybook/react";
-import "../src/index.css";
+import type { Preview } from '@storybook/react';
+import '../src/index.css';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    docs: {
+      canvas: {
+        sourceState: 'shown',
       },
     },
   },
